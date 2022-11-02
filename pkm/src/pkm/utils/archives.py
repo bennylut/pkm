f7516861e4ea9a8ea50154e1280d3f74b74a5b62
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 21:51:11 +0000

    Adding tarfile member sanitization to extractall()

--- pkm/src/pkm/utils/archives.py
+++ pkm/src/pkm/utils/archives.py
@@ -14,9 +14,50 @@ def extract_archive(archive_path: Path, target_directory: Path):
             z.extractall(target_directory)
     elif archive_path.name.endswith('.tar.gz'):
         with tarfile.open(archive_path) as tar:
-            tar.extractall(target_directory)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, target_directory)
     elif archive_path.name.endswith(".tar.bz2"):
         with tarfile.open(archive_path, 'r:bz2') as tar:
-            tar.extractall(target_directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, target_directory)
     else:
         raise UnsupportedOperation(f"does not support archive {archive_path.name}")
